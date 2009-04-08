b2feeea42ffe5384ed518c7f649c7ba4e47f4078
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-04-08 20:37:57 +0000

    SAK-15990 add ECL-2.0 copyright notice.
    git-svn-id: https://source.sakaiproject.org/svn/providers/branches/sakai_2-6-x@59866 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- cm-authz-integration-test/src/test/java/org/sakaiproject/coursemanagement/test/ConfigurationTestBase.java
+++ cm-authz-integration-test/src/test/java/org/sakaiproject/coursemanagement/test/ConfigurationTestBase.java
@@ -1,14 +1,13 @@
 /**********************************************************************************
-*
-* $Id$
-*
-***********************************************************************************
-*
- * Copyright 2008 Sakai Foundation
+ * $URL:$
+ * $Id:$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2008 Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at
  *
  *       http://www.osedu.org/licenses/ECL-2.0
  *
@@ -17,8 +16,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*
-**********************************************************************************/
+ *
+ **********************************************************************************/
 
 package org.sakaiproject.coursemanagement.test;
 
--- cm-authz-integration-test/src/test/java/org/sakaiproject/coursemanagement/test/DefaultConfigurationTest.java
+++ cm-authz-integration-test/src/test/java/org/sakaiproject/coursemanagement/test/DefaultConfigurationTest.java
@@ -1,9 +1,8 @@
 /**********************************************************************************
-*
-* $Id$
-*
-***********************************************************************************
-*
+ * $URL:$
+ * $Id:$
+ ***********************************************************************************
+ *
  * Copyright (c) 2008 Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
@@ -17,8 +16,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*
-**********************************************************************************/
+ *
+ **********************************************************************************/
 
 package org.sakaiproject.coursemanagement.test;
 
